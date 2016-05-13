6e3441ccec3725ced3eb51f48cea142564be4146
lawton browning <lawton.a.browning@gmail.com>
2016-05-13 09:07:24 -0700

    basic skeleton for Twilio Ip app

---
+++ TwilioIpMessaging/Models/EnvironmentVariables.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TwilioIpMessaging.Models
+{
+    public class EnvironmentVariables
+    {
+        public static string AccountSid = "ACb0bfd5d796c5ccb460981071eaef4872";
+        public static string APIkey = "SK08ad296813e016b960490e285fe89360";
+        public static string Secret = "LvaqblFxlY9F8MN4teZRptvYHLxlJunz";
+
+        public static string AuthToken = "b172bd7438ac4ec16fef767540d8b538";
+        public static string TwilioPhone = "+19713402408";
+        public static string MailChimpApi = "bee1bfb378a125a76a07a7ad4328e64e-us13";
+        public static string ServiceSID = "IS519be98a1f264ead947abd94370f91c3";
+
+    }
+}
